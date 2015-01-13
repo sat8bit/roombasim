8cc3978862d7d5c2419d00bf45dca4d8306e4269
sat8bit <sat8bit@gmail.com>
2015-01-13 23:54:55 +0900

    BasicLivingRoom20x15を追加

---
+++ src/RoombaSim/Room/BasicLivingRoom20x15.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace sat8bit\RoombaSim\Room;
+
+class BasicLivingRoom20x15 extends AbstractRoom
+{
+    /**
+     * @var array
+     */
+    protected $map = array(
+        array(1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(0    , 1    , 0    , 0    , 1    , 0    , 0    , 1    , 0    , 2    , 0    , 1    , 0    , 0    , 1    ),
+        array(1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, 1    ),
+        array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, 1    ),
+        array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, 1    ),
+        array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, 0    ),
+        array(false, false, false, false, false, false, false, false, false, false, false, false, false, false, 1    ),
+        array(1    , 1    , 2    , 2    , 2    , 2    , 2    , 3    , 2    , 4    , false, false, false, false, 1    ),
+        array(1    , 1    , 2    , 2    , 2    , 2    , 2    , 2    , 2    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , false, false, false, false, false, 2    , 2    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , false, false, false, false, false, 2    , 2    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , false, false, false, false, false, 2    , 2    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , false, false, false, false, false, 2    , 1    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 3    , false, false, false, false, 1    ),
+        array(1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(1    , false, false, false, false, false, false, false, 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(1    , false, false, false, false, false, false, false, 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+        array(3    , false, false, false, false, false, false, false, 1    , 1    , 1    , 1    , 1    , 1    , 1    ),
+    );
+}
